ba6ec65714d2f74c66c0209fc31974c8596c9eee
shanik hernandez <shaniik.her93@gmail.com>
2020-04-17 23:12:08 -0400

    small details

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -43,3 +43,17 @@ $(".create-form").on("submit", function(event) {
       }
     );
   });
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        
+        location.reload();
+      }
+    );
+  });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
