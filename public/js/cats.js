42bb6286e019e19b153c40f14d97e56293f47151
shanik hernandez <shaniik.her93@gmail.com>
2020-04-17 21:00:29 -0400

    ajax calls

---
+++ public/js/cats.js
@@ -0,0 +1,45 @@
+$(function () {
+    $(".change-devoured").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("newdevoured");
+
+        var newDevouredState = {
+            devoured: newDevoured
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function () {
+                console.log("changed devoured to", newDevoured);
+                location.reload();
+            }
+        );
+    });
+    $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+    }).then(
+        function () {
+            console.log("created new burger");
+            location.reload();
+        }
+    );
+});
+$(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
